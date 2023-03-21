08edb334ddfed6ad4ceb1249eabdf7b313edcabd
leon-do <leondo123@gmail.com>
2023-03-21 16:13:33 -0400

    zapier auth

---
+++ src/pages/zapier/auth.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Response = {
+  success: boolean;
+};
+
+// https://platform.zapier.com/docs/apikey
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
+  try {
+    console.log("/zapier/auth");
+    if (req.body.api_key === process.env.X_API_KEY) return res.status(200).json({ success: true });
+    return res.status(400).json({ success: false });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ success: false });
+  }
+}
